d2d233166cbf2093f0cea7ccc1fed913719cef3a
Raf <rafaellos.g9@gmail.com>
2018-01-09 12:26:50 +0200

    Create lab2_1.c

---
+++ 3. Numerical Analysis - Scientific Application Programming/Lab3-4/Lab2_1/lab2_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+double iparag(int i)
+{
+    float iparag = 1;
+    int j = 1;
+
+    for(j; j <= i; j++)
+    {
+        iparag *= j;
+    }
+
+    return iparag;
+}
+
+int main(int argc, char *argv[])
+{
+    int x = 2, n = 2;
+
+    double myexp = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        myexp += pow(x, i) / iparag(i);
+    }
+
+    printf("x = %d, myexp(%d) = %f, exp(%d) = %f", x, x, myexp, x, exp(x));
+    return 0;
+}
