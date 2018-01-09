91baff731b80f9f3871881ab8671aedb5849ab41
Raf <rafaellos.g9@gmail.com>
2018-01-09 18:32:02 +0200

    Create lab3_4.c

---
+++ 3. Numerical Analysis - Scientific Application Programming/Lab5-7/Lab3_4/lab3_4.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+float Horner2(int n, float p[], float q[], int x)
+{
+    q[0] = p[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        q[i] = p[i] + (q[i - 1] * x);
+    }
+
+    return q[n];
+}
+
+int main(int argc, char *argv[])
+{
+    int n;
+
+    printf("Dose vathmo poluonimou: ");
+    scanf("%d", &n);
+
+    float p[n];
+    float q[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Dose timi gia to p[%d]: ", i);
+        scanf("%f", &p[i]);
+    }
+
+    printf("\nSuntelestes polionimou\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("P[%d] = %f\n", i, p[i]);
+    }
+
+    int x;
+
+    printf("\nDose timi gia to x: ");
+    scanf("%d", &x);
+
+    printf("Timh upoloipou diaireshs = %f\n", Horner2(n, p, q, x));
+
+    printf("\nTimes sunteleston q\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Q[%d] = %f\n", i, q[i]);
+    }
+    return 0;
+}
